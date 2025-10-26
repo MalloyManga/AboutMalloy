e3a4227ea42b609ca0f07460b1acc09cebd1fb46
Malloy <li3210361050@gmail.com>
2025-10-26 23:22:54 +0800

    添加了一个简单的样式 以及对于 discord 的链接的重构

--- app/components/Contact1.vue
+++ app/components/Contact1.vue
@@ -1,39 +1,62 @@
 <!-- Contact1.vue -->
 <script setup lang="ts">
+import feather from '../assets/images/Feather.webp'
 
+const rotationAngle = ref<number>(0)
+const discordText = ref('malloyli')
+
+const handleDiscordClick = async () => {
+    await navigator.clipboard.writeText('malloyli')
+    discordText.value = 'Copied!'
+    rotationAngle.value += 360
+}
+
+watch(discordText, () => {
+    setTimeout(() => {
+        discordText.value = 'malloyli'
+    }, 1000)
+})
 </script>
 
 <template>
-    <div class="back_board size-full pb-2 bg-[#3A4F7F]">
-        <div class="mc-text border-4 size-full bg-[#5371ad] p-4 text-white">
-            <div>
-                <span class="text-2xl mb-2 px-1 bg-[#2A5298] inline-block">
-                    DM me
-                </span>
-            </div>
-            <div class="text-lg px-1 mb-4">
-                Feel free to contact me if you need.
-                <!-- <a href="#" class="underline underline-offset-2">
+    <div class="relative size-full">
+        <img :src="feather" class="absolute z-20 inline-block size-10 bg-pink-30 right-2 -top-4">
+        <div class="back_board size-full pb-2 bg-[#3A4F7F]">
+            <div class="mc-text border-4 size-full bg-[#5371ad] p-4 text-white">
+                <div>
+                    <span class="text-2xl mb-2 px-1 bg-[#2A5298] inline-block">
+                        DM me
+                    </span>
+                </div>
+                <div class="text-lg px-1 mb-4">
+                    Feel free to contact me if you need.
+                    <!-- <a href="#" class="underline underline-offset-2">
                     (e.com)
                 </a> -->
-            </div>
-            <div class="mb-4">
-                I'm currently focused on improving my language skills and may be busy with schoolwork but I
-                will do my best to reply to every email(no spam) as soon as possible.
-            </div>
-            <div class="inline-block bg-[#2A5298] mb-2">
-                I'm also active on these platforms:
-            </div>
-            <div class="flex gap-2">
-                <a href="https://github.com/MalloyManga" target="_blank" class="inline-block">
-                    <IconGithub class="size-12" />
-                </a>
-                <a href="https://okjk.co/6Gx97S" target="_blank" class="inline-block">
-                    <IconJike class="size-12" />
-                </a>
-                <a href="#" target="_blank" class="inline-block">
-                    <IconDiscord class="size-12" />
-                </a>
+                </div>
+                <div class="mb-4">
+                    I'm currently focused on improving my language skills and may be busy with schoolwork but I
+                    will do my best to reply to every email(no spam) as soon as possible.
+                </div>
+                <div class="inline-block bg-[#2A5298] mb-2">
+                    I'm also active on these platforms:
+                </div>
+                <div class="flex gap-2">
+                    <a href="https://github.com/MalloyManga" target="_blank" class="inline-block">
+                        <IconGithub class="size-12" />
+                    </a>
+                    <a href="https://okjk.co/6Gx97S" target="_blank" class="inline-block">
+                        <IconJike class="size-12" />
+                    </a>
+                    <span class="group inline-block relative cursor-pointer" @click="handleDiscordClick">
+                        <IconDiscord class="size-12 transition-transform duration-500"
+                            :style="{ transform: `rotate(${rotationAngle}deg)` }" />
+                        <span
+                            class="text-xs absolute -left-1/12 bg-gray-700 pl-1 rounded-sm opacity-0 transition-all duration-300 group-hover:opacity-100">
+                            {{ discordText }}
+                        </span>
+                    </span>
+                </div>
             </div>
         </div>
     </div>
