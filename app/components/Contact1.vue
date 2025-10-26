c86cfab1743966ee8e9048c978bb60c9810f50f9
Malloy <li3210361050@gmail.com>
2025-10-26 21:15:18 +0800

    css 写法待重构 简单写了一个 contact 的小卡片

---
+++ app/components/Contact1.vue
@@ -0,0 +1,53 @@
+<!-- Contact1.vue -->
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="back_board size-full pb-2 bg-[#3A4F7F]">
+        <div class="mc-text border-4 size-full bg-[#5371ad] p-4 text-white">
+            <div>
+                <span class="text-2xl mb-2 px-1 bg-[#2A5298] inline-block">
+                    DM me
+                </span>
+            </div>
+            <div class="text-lg px-1 mb-4">
+                Feel free to contact me if you need.
+                <!-- <a href="#" class="underline underline-offset-2">
+                    (e.com)
+                </a> -->
+            </div>
+            <div class="mb-4">
+                I'm currently focused on improving my language skills and may be busy with schoolwork but I
+                will do my best to reply to every email(no spam) as soon as possible.
+            </div>
+            <div class="inline-block bg-[#2A5298] mb-2">
+                I'm also active on these platforms:
+            </div>
+            <div class="flex gap-2">
+                <a href="https://github.com/MalloyManga" target="_blank" class="inline-block">
+                    <IconGithub class="size-12" />
+                </a>
+                <a href="https://okjk.co/6Gx97S" target="_blank" class="inline-block">
+                    <IconJike class="size-12" />
+                </a>
+                <a href="#" target="_blank" class="inline-block">
+                    <IconDiscord class="size-12" />
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.mc-text {
+    font-family: 'MC';
+    border-image: url("data:image/svg+xml,%3Csvg width='8' height='8' viewBox='0 0 8 8' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0_1310_2574)'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M4 0H3V1H2V2H1V3H0V4V5H1V6H2V7H3V8H4H5V7H6V6H7V5H8V4V3H7V2H6V1H5V0H4ZM5 1V2H6V3H7V4V5H6V6H5V7H4H3V6H2V5H1V4V3H2V2H3V1H4H5Z' fill='%23475569'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_1310_2574'%3E%3Crect width='8' height='8' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E%0A") 3 / 6px / 0px stretch;
+    clip-path: polygon(6px 0px, 6px 2px, 4px 2px, 4px 4px, 2px 4px, 2px 6px, 0px 6px, 0px 8px, 0px calc(100% - 6px), 2px calc(100% - 6px), 2px calc(100% - 4px), 4px calc(100% - 4px), 4px calc(100% - 2px), 6px calc(100% - 2px), 6px calc(100% + 0px), 8px calc(100% + 0px), calc(100% - 6px) calc(100% + 0px), calc(100% - 6px) calc(100% - 2px), calc(100% - 4px) calc(100% - 2px), calc(100% - 4px) calc(100% - 4px), calc(100% - 2px) calc(100% - 4px), calc(100% - 2px) calc(100% - 6px), calc(100% + 0px) calc(100% - 6px), calc(100% + 0px) calc(100% - 8px), calc(100% + 0px) 6px, calc(100% - 2px) 6px, calc(100% - 2px) 4px, calc(100% - 4px) 4px, calc(100% - 4px) 2px, calc(100% - 6px) 2px, calc(100% - 6px) 0px, calc(100% - 8px) 0px);
+}
+
+.back_board {
+    border-image: url("data:image/svg+xml,%3Csvg width='8' height='8' viewBox='0 0 8 8' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0_1310_2574)'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M4 0H3V1H2V2H1V3H0V4V5H1V6H2V7H3V8H4H5V7H6V6H7V5H8V4V3H7V2H6V1H5V0H4ZM5 1V2H6V3H7V4V5H6V6H5V7H4H3V6H2V5H1V4V3H2V2H3V1H4H5Z' fill='%23475569'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_1310_2574'%3E%3Crect width='8' height='8' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E%0A") 3 / 6px / 0px stretch;
+    clip-path: polygon(6px 0px, 6px 2px, 4px 2px, 4px 4px, 2px 4px, 2px 6px, 0px 6px, 0px 8px, 0px calc(100% - 6px), 2px calc(100% - 6px), 2px calc(100% - 4px), 4px calc(100% - 4px), 4px calc(100% - 2px), 6px calc(100% - 2px), 6px calc(100% + 0px), 8px calc(100% + 0px), calc(100% - 6px) calc(100% + 0px), calc(100% - 6px) calc(100% - 2px), calc(100% - 4px) calc(100% - 2px), calc(100% - 4px) calc(100% - 4px), calc(100% - 2px) calc(100% - 4px), calc(100% - 2px) calc(100% - 6px), calc(100% + 0px) calc(100% - 6px), calc(100% + 0px) calc(100% - 8px), calc(100% + 0px) 6px, calc(100% - 2px) 6px, calc(100% - 2px) 4px, calc(100% - 4px) 4px, calc(100% - 4px) 2px, calc(100% - 6px) 2px, calc(100% - 6px) 0px, calc(100% - 8px) 0px);
+}
+</style>
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,16 +1,13 @@
 <!-- pages/contact.vue -->
 <script setup lang="ts">
 
-
-
-
 </script>
 
 <template>
-
-
-
-
-
+    <div class="lg:h-6/7 grid lg:grid-cols-[min(40%,40rem)_1fr] gap-4 lg:px-16 sm:px-8 px-2">
+        <div class="lg:row-start-1 lg:row-end-2 lg:h-5/6">
+            <Contact1 />
+        </div>
+    </div>
 
 </template>
