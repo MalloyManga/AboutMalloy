3c52f853449e790a471e96a19d508d79ed0a6b4b
Malloy <li3210361050@gmail.com>
2025-10-28 18:26:49 +0800

    微调了 rencent item 的样式 border 以及 clip-path

--- app/components/Recent.vue
+++ app/components/Recent.vue
@@ -29,7 +29,7 @@ const recentActivities: RecentActivity[] = [
             </div>
             <ul class="grid gap-3 max-h-[40vh] overflow-auto py-1">
                 <li v-for="(item, index) in recentActivities" :key="index"
-                    class="recent-item bg-[#608bc1] h-fit py-1 px-2 cursor-pointer hover:bg-[#2a5298]">
+                    class="recent-item border-4 border-[#133e87] bg-[#608bc1] h-fit py-1 px-2 cursor-pointer hover:bg-[#2a5298]">
                     <a v-if="item.link" :href="item.link" class="flex flex-col" target="_blank">
                         <span class="bg-[#2a5298] size-fit px-1 my-1">
                             {{ item.time }}
@@ -57,11 +57,20 @@ const recentActivities: RecentActivity[] = [
     box-shadow: 0 -4px #2a5298 inset,
         -4px 0 #2a5298 inset,
         0 4px #cdefff inset,
-        4px 0 #cdefff inset,
-        -3px 0 #133e87,
-        0 -3px #133e87,
-        3px 0 #133e87,
-        0 3px #133e87;
+        4px 0 #cdefff inset;
+    clip-path: polygon(0px 4px,
+            4px 4px,
+            4px 0px,
+            calc(100% - 4px) 0px,
+            calc(100% - 4px) 4px,
+            100% 4px,
+            100% calc(100% - 4px),
+            calc(100% - 4px) calc(100% - 4px),
+            calc(100% - 4px) 100%,
+            4px 100%,
+            4px calc(100% - 4px),
+            0px calc(100% - 4px));
+
 }
 
 .recent-item:hover {
