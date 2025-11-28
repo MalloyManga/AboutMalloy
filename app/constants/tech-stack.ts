d12e04c3ee873a692290bc156888916089a127f2
Malloy <li3210361050@gmail.com>
2025-11-28 21:44:10 +0800

    更新了技术栈的内容

--- app/constants/tech-stack.ts
+++ app/constants/tech-stack.ts
@@ -8,6 +8,8 @@ import ReactDark from '~/components/icon/ReactDark.vue'
 import NodejsDark from '~/components/icon/NodejsDark.vue'
 import NestDark from '~/components/icon/NestDark.vue'
 import ExpressDark from '~/components/icon/ExpressDark.vue'
+import Electron from '~/components/icon/Electron.vue'
+import Three from '~/components/icon/Three.vue'
 
 export const TECH_STACKS: TechStack[] = [
     // add new tech stack icon and link
@@ -18,5 +20,7 @@ export const TECH_STACKS: TechStack[] = [
     { stack: ReactDark, link: 'https://react.dev/' },
     { stack: NodejsDark, link: 'https://nodejs.org/' },
     { stack: NestDark, link: 'https://nestjs.com/' },
-    { stack: ExpressDark, link: 'https://expressjs.com/' }
+    { stack: ExpressDark, link: 'https://expressjs.com/' },
+    { stack: Electron, link: 'https://www.electronjs.org/' },
+    { stack: Three, link: 'https://threejs.org/' }
 ] as const
